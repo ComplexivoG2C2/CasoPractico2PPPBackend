82bd03df6681d387f2f91099e92370bbfb0a4f5e
Darwin Gonzalez <darwin.gonzalez.est@tecazuay.edu.ec>
2022-06-13 22:23:32 -0500

    Tutores Empresariales

--- src/main/java/com/tecazuay/complexivog2c2/repository/Primary/designaciones/TutorEmpProyectoRepository.java
+++ src/main/java/com/tecazuay/complexivog2c2/repository/Primary/designaciones/TutorEmpProyectoRepository.java
@@ -3,6 +3,7 @@ package com.tecazuay.complexivog2c2.repository.Primary.designaciones;
 
 import com.tecazuay.complexivog2c2.model.Primary.coordinadores.CoordinadorCarrera;
 import com.tecazuay.complexivog2c2.model.Primary.desigaciones.TutorEmp;
+import com.tecazuay.complexivog2c2.model.Primary.empresa.Empresa;
 import com.tecazuay.complexivog2c2.model.Primary.solicitudproyecto.ProyectoPPP;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -14,6 +15,8 @@ public interface TutorEmpProyectoRepository extends JpaRepository<TutorEmp, Long
     Optional<TutorEmp> findByCedula(String cedula);
 
     List<TutorEmp> findByCoordinadorCarrera(CoordinadorCarrera coordinadorCarrera);
+
+    List<TutorEmp> findByEmpresa(Empresa empresa);
 
     Optional<TutorEmp> findByProyectoPPPAndEstado(ProyectoPPP proyectoPPP, boolean estado);
 
