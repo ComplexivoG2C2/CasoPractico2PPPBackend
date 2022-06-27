49457e8ed9bfe146d09987f674847435ba3dc302
LeoGg17 <leoguartambel777@gmail.com>
2022-06-26 21:55:14 -0500

    segurity..

--- src/main/java/com/tecazuay/complexivog2c2/security/SecurityConfig.java
+++ src/main/java/com/tecazuay/complexivog2c2/security/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
-
+//
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
